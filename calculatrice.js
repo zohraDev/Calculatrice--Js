a706a937a68e791a96e7801c8884482646ba5ae1
zohraDev <zohra.bachi@edu-itescia.fr>
2020-12-29 17:53:55 +0100

    premier commit

---
+++ calculatrice.js
@@ -0,0 +1,20 @@
+/*   -----------Calculatrice-----------  */
+
+
+var nombre1;
+var Nombre2;
+var choix;
+
+choix= prompt('Que-souhaitez-vous faire ?\n '
+           + '1- Addition \n'
+           + '2- Multipilication\n'
+           + '3- Soustraction\n' 
+           + '4- Division \n\n');
+
+
+switch (choix){
+    case '1': 
+
+
+}           
+
