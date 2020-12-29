e0bf19ce896a0ecd252281c9d9da2856fd766a9d
zohraDev <zohra.bachi@edu-itescia.fr>
2020-12-29 21:02:22 +0100

    'Ajout boucle do dans le cas où l'utilisateur a saisi autre qu'un nombre'

--- calculatrice.js
+++ calculatrice.js
@@ -7,38 +7,55 @@ var choix;
 
 /*-----------Vérification de choix */
 do{
-    do {
     
-        choix = prompt('Que-souhaitez-vous faire ?\n '
-            + '1- Addition.\n'
-            + '2- Soustraction.\n'
-            + '3- Multipilication\n'
-            + '4- Soustraction.\n' 
-            + '5- Division.\n\n');
+        do{
         
+            choix = prompt('Que-souhaitez-vous faire ?\n '
+                + '1- Addition.\n'
+                + '2- Soustraction.\n'
+                + '3- Multipilication\n'
+                + '4- Division.\n\n'); 
+            
 
-            if(choix != 1 && choix != 2 && choix != 3 && (choix != 4) ){
+                if(choix != 1 && choix != 2 && choix != 3 && (choix != 4) ){
 
-            alert("Erreur de saisi dans le choix de l\'opération");}
+                alert("❌ Erreur de saisi dans le choix de l\'opération");
+            }
+            
 
-    }while ( (choix != 1) && (choix != 2) && (choix != 3)&& (choix != 4)  );
+        }while ( (choix != 1) && (choix != 2) && (choix != 3)&& (choix != 4)  );
 
     /*--------------------------------------- */ 
 
 
     /* ------- Controler la division par 0 --------*/
-    nombre1 =Number(prompt('Entrez le premier nombre ;'));
+    do{
+        
+        nombre1 =Number(prompt('Entrez le premier nombre ;'));
 
+        if( isNaN(nombre1)) alert('❌ Vous devez entrer un nombre');
+
+    
+    } while(isNaN(nombre1));
+          
 
     do{
-        nombre2 = Number(prompt('Entrez le deuxième nombre :'));
-        if(nombre2==0){
+          nombre2 = Number(prompt('Entrez le deuxième nombre :'));
 
-            alert('On n epeut pas diviser par 0.')
-        }
+          while(choix==4 && nombre2==0){
 
-    }while(choix==4 && nombre2==0);
+            if(nombre2==0){
 
+                alert('❌ On ne peut pas diviser par 0 .')
+            }
+
+            nombre2 = Number(prompt('Entrez le deuxième nombre :'));
+
+          };
+          if( isNaN(nombre2)) alert('❌ Vous devez entrer un nombre');
+
+
+    }while(isNaN(nombre2));
 
 
 
